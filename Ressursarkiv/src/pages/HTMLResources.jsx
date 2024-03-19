b60f71262a1042c2477ffb1a966cf5b3b96a407b
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-03-19 15:52:48 +0100

    La inn start av routing i pages, lagde html og javascript

---
+++ Ressursarkiv/src/pages/HTMLResources.jsx
@@ -0,0 +1,7 @@
+import { resources } from '../assets/ressurser';
+
+export default function HTMLResources(){
+
+    const ResourcesHTML = resources.filter(resource => resource.category === "HTML");
+    console.log("html", ResourcesHTML)
+}
---
+++ Ressursarkiv/src/pages/JavascriptResources.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { resources } from './resources';
+
+export default function JavascriptResources(){
+const javascriptResources = resources.filter(resource => resource.category === "JavaScript");
+}
