f09835f7f4f4a709b1a8823e8cf081436e8e3f0f
Riddy366 <ridwanabukar851@gmail.com>
2024-03-21 12:42:12 +0100

    mmerge

--- Ressursarkiv/src/Components/Layout.jsx
+++ Ressursarkiv/src/Components/Layout.jsx
@@ -1,30 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function Layout({ resources }) {
+  function selectResource(index) {
+    const resource = resources[index];
+    const resourceContent = document.getElementById('resourceContent');
+    resourceContent.innerHTML = `
+      <h2>${resource.category}</h2>
+      <p>${resource.text}</p>
+      ${resource.sources.map(source => `<a href="${source.url}" target="_blank">${source.title}</a>`).join("<br/>")}
+    `;
+  }
     return (
       <div className="center">
         <nav>
           <ul id="resourceList">
             {resources.map((resource, index) => (
               <li key={index}>
-                <button onClick={() => selectResource(index)}>{resource.category}</button>
+                <Link to={resource.category}><button onClick={() => selectResource(index)}>{resource.category}</button></Link>
               </li>
             ))}
           </ul>
         </nav>
         
         <article id="resourceContent">
-
+  
         </article>
       </div>
     );
   
-    function selectResource(index) {
-      const resource = resources[index];
-      const resourceContent = document.getElementById('resourceContent');
-      resourceContent.innerHTML = `
-        <h2>${resource.category}</h2>
-        <p>${resource.text}</p>
-        ${resource.sources.map(source => `<a href="${source.url}" target="_blank">${source.title}</a>`).join("<br/>")}
-      `;
-    }
+    
   }
   
