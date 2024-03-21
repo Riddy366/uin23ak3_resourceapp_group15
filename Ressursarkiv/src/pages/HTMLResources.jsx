0b150cb4d88afc777e802db2c298b6114d9aee4e
Riddy366 <ridwanabukar851@gmail.com>
2024-03-21 09:30:45 +0100

    fix

---
+++ Ressursarkiv/src/pages/HTMLResources.jsx
@@ -0,0 +1,8 @@
+import { resources } from '../assets/ressurser';
+
+export default function HTMLResources(){
+
+    const ResourcesHTML = resources.filter(resource => resource.category === "HTML");
+    console.log("html", ResourcesHTML)
+    
+} 
