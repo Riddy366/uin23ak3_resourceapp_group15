3755404bbf5eab7bc3139a323f982008d72be9c5
abdalla162 <abdalla162@hotmail.com>
2024-03-21 10:15:55 +0100

    update

--- Ressursarkiv/src/pages/HTMLResources.jsx
+++ Ressursarkiv/src/pages/HTMLResources.jsx
@@ -4,4 +4,7 @@ export default function HTMLResources(){
 
     const ResourcesHTML = resources.filter(resource => resource.category === "HTML");
     console.log("html", ResourcesHTML)
+
+    const ResourcesCSS = resources.filter(resource => resource.category === "CSS");
+    console.log("css", ResourcesCSS)
 }
